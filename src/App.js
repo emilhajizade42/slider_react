c3fe922425febfc586fb7897b026c097cc7af33e
emilhajizade <emilhacizade44@mail.ru>
2022-09-21 06:56:20 +0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import axios from 'axios';
-import { useState } from 'react';
+// import axios from 'axios';
+// import { useState } from 'react';
 import './App.css';
 import ImageSlider from './ImageSlider';
 
 function App() {
   const slides = [
     { url: "https://coffee.alexflipnote.dev/random", title: "beach" },
-    { url: "https://coffee.alexflipnote.dev/random", title: "boat" },
-    { url: "https://coffee.alexflipnote.dev/random", title: "forest" },
-    { url: "https://coffee.alexflipnote.dev/random", title: "city" },
-    { url: "https://coffee.alexflipnote.dev/random", title: "italy" },
+    { url: "https://picsum.photos/id/237/200/300", title: "boat" },
+    { url: "https://picsum.photos/id/217/200/300", title: "forest" },
+    { url: "https://picsum.photos/id/216/200/300", title: "city" },
+    { url: "https://picsum.photos/id/219/200/300", title: "italy" },
   ];
-  const [render, setrender] = useState(true)
-  setInterval(() => {
-    slides.forEach((item)=>{
+  // const [render, setrender] = useState(true)
+  // setInterval(() => {
+  //   slides.forEach((item)=>{
 
-      axios.get("https://coffee.alexflipnote.dev/random")
-      .then(res=>item.url = res.data)
+  //     axios.get("https://coffee.alexflipnote.dev/random")
+  //     .then(res=>item.url = res.data)
 
-    });
-    setrender(!render)
+  //   });
+  //   setrender(!render)
 
-  },3000)
+  // },3000)
   const containerStyles = {
     width: "500px",
     height: "280px",
